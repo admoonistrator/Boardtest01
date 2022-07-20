73418af3fa1bd4c738ff9dc760eb6dc4c46e0e6d
admoonistrator <91christian83@gmail.com>
2022-07-20 09:34:35 +0900

    BoardIn.jsp BoardDAO
    oneBoard

--- A0718/src/main/java/test/BoardDAO.java
+++ A0718/src/main/java/test/BoardDAO.java
@@ -134,5 +134,47 @@ getCon();
 		}
 		return v;
 	}
+public BoardBean oneBoard(int num) {
+		
+		BoardBean bean=new BoardBean();
+		
+		getCon();
+		
+		try {
+			String readsql="update board set readcount=readcount+1 where num=?";
+			pstmt=con.prepareStatement(readsql);
+			pstmt.setInt(1, num);
+			
+			pstmt.executeUpdate();
+			
+			String sql="select *from board where num=?";
+			//DB에 값을 가져와 브라우저에 뿌려야함
+			
+			pstmt=con.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			
+			rs=pstmt.executeQuery();
+			
+			if(rs.next()) {
+				bean.setNum(rs.getInt(1));
+				bean.setWriter(rs.getString(2));
+				bean.setEmail(rs.getString(3));
+				bean.setSubject(rs.getString(4));
+				bean.setPassword(rs.getString(5));
+				bean.setReg_date(rs.getDate(6).toString());
+				bean.setRef(rs.getInt(7));
+				bean.setRe_step(rs.getInt(8));
+				bean.setRe_level(rs.getInt(9));
+				bean.setReadcount(rs.getInt(10));
+				bean.setContent(rs.getString(11));	
+				
+			}
+			con.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return bean;
+	
+	}
 
 }
